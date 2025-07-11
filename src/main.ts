144762db83c61779fe7de9baabc07514ca461923
hiimstevejin <jingle5695@gmail.com>
2025-07-11 17:51:18 +0900

    add simple layout and html injection logic in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log("Popup script loaded!");
+import "./style.css";
+import mainViewHtml from "./views/main-view.html?raw";
+import settingsViewHtml from "./views/settings-view.html?raw";
+
+const appContainer = document.querySelector<HTMLDivElement>("#app-container");
+
+function showMainView() {
+  if (!appContainer) return;
+  appContainer.innerHTML = mainViewHtml;
+
+  const goToSettingsBtn = document.querySelector<HTMLButtonElement>(
+    "#settingsBtn"
+  );
+  goToSettingsBtn?.addEventListener("click", showSettingsView);
+}
+
+function showSettingsView(){
+  if(!appContainer) return;
+  appContainer.innerHTML = settingsViewHtml;
+
+  const goBackBtn = document.querySelector<HTMLButtonElement>('#go-back-btn');
+  goBackBtn?.addEventListener('click',showMainView);
+}
+
+showMainView();
