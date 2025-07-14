389ed6970569eff4a22fa08cadfe18fed6fb008b
hiimstevejin <jingle5695@gmail.com>
2025-07-15 00:28:50 +0900

    add logic to extract coding language on problem page

--- src/content.ts
+++ src/content.ts
@@ -9,7 +9,35 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
     const problemTitle =
       document.querySelector(".text-title-large")?.textContent;
     const problemDescription = document.querySelector(".elfjS")?.textContent;
-    sendResponse({ title: problemTitle, description: problemDescription });
+
+    // extract the language user selected on leetcode problem editor
+    const langButton = Array.from(
+      document.querySelectorAll('button[aria-haspopup="dialog"]')
+    ).find((btn) =>
+      btn.textContent
+        ?.trim()
+        ?.match(
+          /^(C\+\+|Java|Python3?|C#|JavaScript|TypeScript|PHP|Swift|Kotlin|Dart|Go|Ruby|Scala|Rust|Racket|Erlang|Elixir)/
+        )
+    );
+
+    let problemCodingLanguage = "";
+
+    if (langButton) {
+      const textNode = Array.from(langButton.childNodes).find(
+        (node) => node.nodeType === Node.TEXT_NODE && node.textContent?.trim()
+      );
+      problemCodingLanguage = textNode?.textContent?.trim() || "Python3"; // If Failed fall back on python3
+      console.log("Selected language:", problemCodingLanguage);
+    } else {
+      console.warn("Language selector button not found");
+    }
+
+    sendResponse({
+      title: problemTitle,
+      description: problemDescription,
+      codingLanguage: problemCodingLanguage,
+    });
   } else if (request.type === "HIGHLIGHT_PROBLEM") {
     const problemDescriptionElement = document.querySelector(".elfjS");
     if (problemDescriptionElement) {
